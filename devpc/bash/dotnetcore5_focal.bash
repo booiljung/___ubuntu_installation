39c556b9d7b0ec4d721caeb3787eb6db690fe8c4
booiljung <booiljung@me.com>
2021-06-21 16:30:52 +0900

    dotnet core 5.0 installation

--- devpc/bash/dotnetcore5_focal.bash
+++ devpc/bash/dotnetcore5_focal.bash
@@ -6,3 +6,27 @@ sudo add-apt-repository universe
 sudo apt-get install apt-transport-https -y
 sudo apt-get update
 sudo apt-get install dotnet-sdk-5.0 -y
+
+dotnet tool install --global dotnet-ef
+dotnet tool update --global dotnet-ef
+
+dotnet tool install --global dotnet-aspnet-codegenerator
+dotnet tool update --global dotnet-aspnet-codegenerator
+
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2019.list)"
+sudo apt-get update
+sudo apt-get install -y mssql-server
+sudo /opt/mssql/bin/mssql-conf setup
+systemctl status mssql-server --no-pager
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+sudo apt-get update 
+sudo apt-get install mssql-tools unixodbc-dev
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
+# sqlcmd -S localhost -U SA -P '<YourPassword>'
+
