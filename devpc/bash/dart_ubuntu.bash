a4cf643d126159f8ed4b438de5c0c4ce61db8c9a
booiljung <booiljung@me.com>
2021-06-28 11:51:00 +0900

    change dart

--- devpc/bash/dart_ubuntu.bash
+++ devpc/bash/dart_ubuntu.bash
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 # dart
-sudo apt update -y
-sudo apt install -y curl
-sudo bash -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
-sudo bash -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
 sudo apt-get update -y
-sudo apt-get install -y dart
+sudo apt-get install apt-transport-https -y
+sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+sudo apt-get update
+sudo apt-get install dart -y
 
-# flutter
-curl -o ~/linspace/opensource/flutter_linux_v1.9.1+hotfix.2-stable.tar.xz https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v1.9.1+hotfix.2-stable.tar.xz
-tar xf ~/linspace/opensource/flutter_linux_v1.9.1+hotfix.2-stable.tar.xz -C ~/linspace/opensource/
-
-cat dart_ubuntu_profile.txt >> ~/.bashrc
-
-/usr/lib/dart/bin/pub global activate aqueduct
+cat dart_ubuntu.txt >> ~/.bashrc
 
---
+++ devpc/bash/flutter_ubuntu.bash
@@ -0,0 +1,7 @@
+# flutter
+export PATH="$PATH":"$HOME/linspace/opensource/flutter/bin/"
+
+# flutter webdev
+export PATH="$PATH":"$HOME/.bin/cache/dart-sdk/bin"
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+
